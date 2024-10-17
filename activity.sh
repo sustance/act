f357cadec28e456526c9b6877b1d40b50fbc81b8
Kym Michael <kym.michael@gmail.com>
2024-10-17 10:04:10 +0800

    Update activity.sh

--- activity.sh
+++ activity.sh
@@ -44,7 +44,7 @@ printf "]%s %-15.15s %s %-5s %-5s %-5s %-8s \n" \
 
 printf "|%s %s %s %s %s %s\n" \
 	"${C_ID:-N}" \
- 	"$(command -v crontab >/dev/null 2>&1 && echo "cron"|| echo "<s>cron</s>")" \
+ 	"$(command -v crontab >/dev/null 2>&1 && echo "cron"  || echo "<s>cron</s>")" \
 	"$(command -v w3m     >/dev/null 2>&1 && echo "w3m"   || echo "<s>w3m</s>")" \
 	"$(command -v lynx    >/dev/null 2>&1 && echo "lynx"  || echo "<s>lynx</s>")" \
 	"$(command -v links   >/dev/null 2>&1 && echo "links" || echo "<s>links</s>")" \
@@ -56,7 +56,7 @@ printf "|%s %s %s %s %s %s\n" \
 printf "|%s %s %s\n" \
         "${C_ID:-N}" \
 	"$(command -v newsboat >/dev/null 2>&1 && echo "newsboat"|| echo "<s>newsboat</s>")" \
- 	"$(command -v weechat  >/dev/null 2>&1 && echo "weechat" || echo "<s>weechat</s>")" 
+ 	"$(command -v weechat  >/dev/null 2>&1 && echo "weechat"|| echo "<s>weechat</s>")" 
  
 printf "[%s %s\n" \
 	   "${C_ID:-N}" \
@@ -95,6 +95,7 @@ printf "   %-8s %-8s %-3s %-3s %-3s %-3s\n" \
 		# Print the result in red
 		printf "</pre>\n\n<p style='color:red;'>%s</p>" "$last_access"
     fi 
+
 } > "$o"
 
 cat "$o"
