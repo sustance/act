b82076569c271225a34ebe317800249f029b127c
Kym Michael <kym.michael@gmail.com>
2024-12-11 03:09:06 +0800

    Update do.sh

--- do.sh
+++ do.sh
@@ -64,20 +64,9 @@ printf "|%s %s %s %s %s %s %s %s %s\n" \
 	"$(command -v curl     >/dev/null 2>&1 && echo "curl"   || echo "<s>curl</s>")" \
 	"$(command -v git      >/dev/null 2>&1 && echo "git"    || echo "<s>git</s>")" \
 	"$(command -v ruby     >/dev/null 2>&1 && echo "ruby"   || echo "<s>ruby</s>")" \
-        "$(command -v jekyll   >/dev/null 2>&1 && echo "jekyll" || echo "<s>jekyll</s>")" \
- #       "$(command -v bundler >/dev/null 2>&1 && echo "bundler" || echo "<s>bundler</s>")" \
-	"$(command -v newsboat >/dev/null 2>&1 && echo "newsb"  || echo "<s>newsb</s>")" \
+        "$(command -v jekyll   >/dev/null 2>&1 && echo "jekyll" || echo "<s>jeky</s>")" \
+	"$(command -v newsboat >/dev/null 2>&1 && echo "newsb"  || echo "<s>nwsb</s>")" \
  	"$(command -v weechat  >/dev/null 2>&1 && echo "weec "  || echo "<s>weec</s>")"
- 
-	#"$(command -v finger >/dev/null 2>&1 && echo "finger"  || echo "<s>finger</s>")" \
-	#"$(command -v irssi  >/dev/null 2>&1 && echo "irssi"   || echo "<s>irssi</s>")"
-
-#printf "|%s %s %s %s %s\n" \
-#        "${C_ID:-N}" \
-#	"$(command -v git     >/dev/null 2>&1 && echo "git"   || echo "<s>git</s>")" \
-#	"$(command -v ruby    >/dev/null 2>&1 && echo "ruby"  || echo "<s>ruby</s>")" \
-#	"$(command -v newsboat >/dev/null 2>&1 && echo "newsboat"|| echo "<s>newsboat</s>")" \
-# 	"$(command -v weechat >/dev/null 2>&1 && echo "weechat"|| echo "<s>weechat</s>")" 
  
 printf "[%s %s\n" \
 	"${C_ID:-N}" \
