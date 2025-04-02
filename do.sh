482cd385cf161165693b7fce19549014d7f26d5f
Kym Michael <kym.michael@gmail.com>
2025-04-02 18:14:38 +0800

    Update do.sh

--- do.sh
+++ do.sh
@@ -32,21 +32,22 @@ printf "]%s %-10.10s %-3s %-3s %-3s %-3s %-3s %-3s\n"\
     "$(basename "$SHELL")" 
     # D, P ok $HOSTNAME.... C, E, J, O, T, S  OK cat /proc....
 
-
-printf "|%s %s %s %s %s %s %s %s %s %s\n" \
+printf "<%s %s %s %s %s %s %s %s\n" \
 	"${C_ID:-N}" \
 	"$(command -v w3m      >/dev/null 2>&1 && echo "w3m" || echo "<s>w3m</s>"  )" \
 	"$(command -v lynx     >/dev/null 2>&1 && echo "lynx"|| echo "<s>lynx</s>" )" \
-	"$(command -v git      >/dev/null 2>&1 && echo "git" || echo "<s>git</s>"  )" \
     	"$(command -v jekyll   >/dev/null 2>&1 && echo "jek" || echo "<s>jek</s>"  )" \
 	"$(command -v newsboat >/dev/null 2>&1 && echo "nwsb"|| echo "<s>nwsb</s>" )" \
  	"$(command -v weechat  >/dev/null 2>&1 && echo "wee" || echo "<s>wee</s>"  )" \
   	"$(command -v tldr     >/dev/null 2>&1 && echo "tldr"|| echo "<s>tldr</s>" )" \
-  	"$(command -v fzf      >/dev/null 2>&1 && echo "fzf" || echo "<s>fzf</s>"  )" \
+  	"$(command -v fzf      >/dev/null 2>&1 && echo "fzf" || echo "<s>fzf</s>"  )" 
+    
+printf "|%s %s %s %s\n" \
+	"${C_ID:-N}" \
+	"$(command -v git      >/dev/null 2>&1 && echo "git" || echo "<s>git</s>"  )" \
      	"$(command -v rtorrent >/dev/null 2>&1 && echo "rto" || echo "<s>rto</s>"  )" \
    	"$(command -v ${HOME}/.local/bin/tgpt >/dev/null 2>&1 && echo "tgpt" || echo "<s>tgpt</s>")"
 
- 
 printf "[%s %s\n" \
 	"${C_ID:-N}" \
 	"$(grep "^$(whoami):" /etc/passwd | \
