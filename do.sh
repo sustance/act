cada2fe7f16f3085bb3dc0f27eeadfd8b4c82df4
Kym Michael <kym.michael@gmail.com>
2025-04-20 16:54:26 +0800

    Update do.sh

--- do.sh
+++ do.sh
@@ -66,8 +66,8 @@ printf "(%s %-8s %-8s |dn.%-3s hk.%-3s av.%-3s %-3s<br>\n" \
     "${C_ID:-N}" \
     "$(TZ=UTC-8 date +'%H:%M/%d' 2>/dev/null || date +'%H:%M/%d')" \
     "$(date +'%H:%M/%d')" \
-    "$(ping -c 1 -w 1  8.8.8.8 >/dev/null 2>&1 && ping -c 3  8.8.8.8 2>/dev/null| awk -F'/' 'END {printf "%.0f\n", $5}')"
-    "$(ping -c 1 -w 1 hktv.com >/dev/null 2>&1 && ping -c 3 hktv.com 2>/dev/null| awk -F'/' 'END {printf "%.0f\n", $5}')"
+    "$(ping -c 1 -W 1  8.8.8.8 >/dev/null 2>&1 && ping -c 3  8.8.8.8 2>/dev/null| awk -F'/' 'END {printf "%.0f\n", $5}')"
+    "$(ping -c 1 -W 1 hktv.com >/dev/null 2>&1 && ping -c 3 hktv.com 2>/dev/null| awk -F'/' 'END {printf "%.0f\n", $5}')"
     "$overall_avg" \
     "$total_count"
 
